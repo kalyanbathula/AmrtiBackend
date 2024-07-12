bf97e73b5654f0f2bf68d5185655cd424a20f445
kalyan <kalyanbathula7893@gmail.com>
2024-07-12 14:05:31 +0530

    commit

--- amrti back/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const mongoDbUrl =
-  "mongodb+srv://harshpahwa74:jyFBADBhVAYXhCaZ@cluster0.06c31bq.mongodb.net/?retryWrites=true&w=majority";
-const connectDb=()=>{
-    return mongoose.connect(mongoDbUrl)
-}
-
-module.exports={connectDb}
